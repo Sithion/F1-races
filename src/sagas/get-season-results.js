61292e67046a7d30abab70fe097c89bbf889ee17
Raphael Torres <rrdtorres@gmail.com>
2018-12-11 12:02:23 -0200

    improvements in get-season-results saga for not request api when have the race data

--- src/sagas/get-season-results.js
+++ src/sagas/get-season-results.js
@@ -1,27 +1,36 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import {
+		takeEvery,
+		call,
+		put,
+		select,
+} from 'redux-saga/effects';
+import selectors from '@selectors';
 import Api from '@utils/api';
 
 import actions from '@actions';
 
 function* getSeasonResults(selectedYear) {
-    try {
-        yield put(actions.setLoading(true));
-        const seasonSummary = yield call(Api.get,
-            `http://ergast.com/api/f1/${selectedYear}/results.json?limit=1000`);
-        const { Races } = seasonSummary.MRData.RaceTable;
-        const seasonStanding = yield call(Api.get,
-            `http://ergast.com/api/f1/${selectedYear}/driverStandings.json`);
-        const championId = seasonStanding.MRData.StandingsTable.StandingsLists[0]
-            .DriverStandings[0].Driver.driverId;
-        yield put(actions.setSeason(selectedYear, Races, championId));
-    } catch (e) {
-        console.log('error on getting season results', e);
-    } finally {
-        yield put(actions.setLoading(false));
-    }
+		try {
+				const season = yield select(state => selectors.getSelectedSeason(state));
+				if (!season.races) {
+						yield put(actions.setLoading(true));
+						const seasonSummary = yield call(Api.get,
+								`http://ergast.com/api/f1/${selectedYear}/results.json?limit=1000`);
+						const { Races } = seasonSummary.MRData.RaceTable;
+						const seasonStanding = yield call(Api.get,
+								`http://ergast.com/api/f1/${selectedYear}/driverStandings.json`);
+						const championId = seasonStanding.MRData.StandingsTable.StandingsLists[0]
+								.DriverStandings[0].Driver.driverId;
+						yield put(actions.setSeason(selectedYear, Races, championId));
+				}
+		} catch (e) {
+				console.log('error on getting season results', e);
+		} finally {
+				yield put(actions.setLoading(false));
+		}
 }
 
 export default function* () {
-    yield takeLatest(actions.types.SELECT_YEAR,
-        ({ payload }) => getSeasonResults(payload.year));
+		yield takeEvery(actions.types.SELECT_YEAR,
+				({ payload }) => getSeasonResults(payload.year));
 }
